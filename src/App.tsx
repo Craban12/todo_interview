b5b801ef290f323cf51c6589f840caa39154cbfc
Craban <Antipov-sava@yandex.ru>
2022-10-31 15:19:10 +0300

    Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+import { useReducer } from "react";
+import { Action, State, ContextState } from "./types/stateType";
+import { todoReducer } from "./reducers/todoReducer";
+import NewTask from "./NewTask";
+import TasksList from "./TasksList";
+
+export const initialState: State = {
+  newTask: '',
+  tasks: []
+}
+
+// <Partial> allows you to create the context without default values.
+export const ContextApp = React.createContext<Partial<ContextState>>({});
+
+const App: React.FC = () => {
+
+  const [state, changeState] = useReducer<React.Reducer<State, Action>>(todoReducer, initialState);
+
+  const ContextState: ContextState = {
+    state,
+    changeState
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>
+        React + TypeScript
+      </h1>
+      <ContextApp.Provider value={ContextState}>
+        <NewTask />
+        <TasksList />
+      </ContextApp.Provider>
+    </>
+  )
 }
 
 export default App;
